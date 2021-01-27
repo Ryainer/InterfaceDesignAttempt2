9372874bdcd389b69e1fa321aee4a35871107236
Ryainer <Jonathanjiale@gmail.com>
2021-01-27 17:28:41 +0800

    test

---
+++ InterfaceDesign/Assets/Scripts/ColorButtonScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorButtonScript : MonoBehaviour
+{
+    public GameObject slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void onBtnPress()
+    {
+        if(slider.activeInHierarchy)
+        {
+            slider.SetActive(false);
+        }
+        else if(!slider.activeInHierarchy)
+        {
+            slider.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ InterfaceDesign/Assets/Scripts/SliderScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+    public GameObject car;
+    public Slider slider;
+
+    GameObject carModel;
+
+    Color32 startColor = new Color32(0, 0, 0, 255);
+    Color32 endColor = new Color32(233, 233, 233, 255);
+
+    //Renderer obj;
+
+    private void Start()
+    {
+
+        carModel = Instantiate(car);
+    }
+
+    private void Update()
+    {
+
+        carModel.GetComponentInChildren<Renderer>().material.color = Color.Lerp(startColor, endColor, slider.value / slider.maxValue); 
+    }
+}
